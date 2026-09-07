de1791453814f61b18570dc4b8022c085d755adc
Matta <155326995+Mat6577@users.noreply.github.com>
2026-09-07 14:07:52 +0200

    Update Makefile to include more source files
    Added additional source files for compilation.

--- Makefile.mk
+++ Makefile.mk
@@ -2,6 +2,20 @@ CXX = g++
 CXXFLAGS = -std=c++11 -g -Wall
 
 SOURCES = main.cpp \
+		  AbstractFilmCategories.cpp\
+          Comedy.cpp\
+          Drama.cpp\
+          Film.cpp\
+          FilmCategories.cpp\
+          FilmCategoriesIterator.cpp\
+          FilmIterator.cpp\
+          FilmTypes.cpp\
+          Genres.cpp\
+          Horror.cpp\
+          Romance.cpp\
+          StateBasedIterator.cpp\
+          States.cpp\
+	      Thriller.cpp
 
 TARGET =  taskforge
 
