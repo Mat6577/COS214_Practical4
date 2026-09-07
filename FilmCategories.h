c000a7088c1fba70921454736e57346eb20e61cc
N-Githu <u24607917@tuks.co.za>
2026-09-07 19:29:16 +0200

    Updated files

--- FilmCategories.h
+++ FilmCategories.h
@@ -5,14 +5,17 @@
 
 class FilmCategories : public AbstractFilmCategories
 {
+private:
+    std::string categoryName;
 public:
-    FilmCategories();
+    FilmCategories(std::string name);
     ~FilmCategories() override;
 
     std::string getMetaData() override;
     void printMetaData() override;
 
     FilmIterator* createIterator() override;
+    FilmIterator* createStateIterator(const std::string& state);
 };
 
 #endif
